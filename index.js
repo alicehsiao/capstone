a655d50642094cb01ec895b232c12ef13424430b
Alice Hsiao <alice.hsiao87@gmail.com>
2019-01-01 10:56:42 -0800

    Set up dotenv and env files

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 console.log('Hello');
 
 const test = {
@@ -7,3 +9,5 @@ const test = {
 const redo = { ...test }
 redo["age"] = 31;
 console.log(redo);
+
+console.log(process.env.DB_HOST);
