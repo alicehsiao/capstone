ed7522a0bdc206b8bb3f35f69e4a485788020b4f
Alice Hsiao <alice.hsiao87@gmail.com>
2019-01-17 11:50:16 -0800

    Edited env file

--- start.js
+++ start.js
@@ -21,3 +21,6 @@ app.set('port', process.env.PORT || 7777);
 const server = app.listen(app.get('port'), () => {
     console.log(`Express running → PORT ${server.address().port}`);
 });
+
+// Start script
+// "start": "nodemon --exec babel-node start.js"
